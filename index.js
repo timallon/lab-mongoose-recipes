d92e269adde216a874b9b9da59d8a178140aa7ec
Tim Allon <timallon@gmail.com>
2022-10-28 00:02:23 +0100

    iteration-3

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const Recipe = require('./models/Recipe.model');
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+    const recipe1 = {
+      title: "Chicken Nuggets",
+      level: "UltraPro Chef",
+      ingredients: ["700ml of vegetable oil", "50g of plain flour", "300g of breadcrumbs", "10 chicken nuggets"],
+      cuisine: "American",
+      dishType: "breakfast",
+      image: "https://images.unsplash.com/photo-1562967916-eb82221dfb92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
+      duration: 10,
+      creator: "Tim Allon",
+     created: 2022-03-23,
+    };
+// SORRY - SPENT ALL EVENING TRYING TO CONNECT THE DB TO COMPASS, BUT I DON'T THINK 
+// MONGODB IS DESIGNED TO WORK WITH WSL2.
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -16,8 +28,20 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+   return Recipe.create(recipe1);
+   
+    //model.create
+
+
+
     // Run your code here, after you have insured that the connection was made
+  })
+  .then((result) => {
+    console.log(result);
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  
